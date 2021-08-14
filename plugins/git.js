49b377a8197ed004bea1bccb0e7830a081fe8f68
JOKER_SER <87939150+J0kerser@users.noreply.github.com>
2021-08-14 08:17:23 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://tinyurl.com/ykxwd25q";
+  r_text[0] = "https://i.imgur.com/s4drNeU.jpeg";
     
     
     var i = Math.floor(1*Math.random())
